54104250b8e5448a3a6ab7396f1af82e5611ccc3
Makar <makar.melikyan@gmail.com>
2021-12-06 01:01:30 +0400

    eslint installed

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    // v.1.5 (2021-10-14)
+    '@typescript-eslint/no-explicit-any': 0,
+    'arrow-parens': [1, 'as-needed'],
+    'brace-style': [1, 'stroustrup', { allowSingleLine: true }],
+    'class-methods-use-this': 0,
+    'func-names': [1, 'always', { generators: 'never' }],
+    'import/extensions': 0,
+    'import/prefer-default-export': 0,
+    'no-console': [1, { allow: ['error', 'info', 'warning'] }],
+    'no-return-assign': 0,
+    'linebreak-style': 0,
+    'object-curly-newline': [
+      0,
+      {
+        ObjectExpression: 'always',
+        ObjectPattern: { minProperties: 2 },
+      },
+    ],
+    'no-plusplus': [1, { allowForLoopAfterthoughts: true }],
+
+    // React specific
+    'react/jsx-one-expression-per-line': [1, { allow: 'single-child' }],
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
+  }
+,
+};
